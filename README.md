3ae48bdccbf9b044f9e3d362774b3c057e2d8739
Enes Kurbetoğlu <eneskurbetoglu1@gmail.com>
2023-05-04 11:02:31 +0300

    add base currency + requirements

--- README.md
+++ README.md
@@ -1,12 +1,19 @@
 # How poor am I?
 
-Tells you how poor you are in a certain currency given all your other currencies.
+Tells you how poor you are in a certain currency given all your other currencies and XAU. 
+w/ [CollectAPI](https://collectapi.com/).
+
+## Usage
+
+```
+$ ./calc.py -b BASE_CURRENCY_CODE [-a] [FLOAT_AMOUNT] [-c] [CURRENCY_CODE] [-a] [FLOAT] [-c] [CURRENCY_CODE] ...
+```
+```
+python3 calc.py -b BASE_CURRENCY_CODE [-a] [FLOAT_AMOUNT] [-c] [CURRENCY_CODE] [-a] [FLOAT_AMOUNT] [-c] [CURRENCY_CODE] ...
+```
 
 ## Example
 
 ```
-$ ./calc.py [-a] [FLOAT] [-c] [CODE] [-a] [FLOAT] [-c] [CODE] ...
-```
-```
-python calc.py [-a] [FLOAT] [-c] [CODE] [-a] [FLOAT] [-c] [CODE] ...
+python3 calc.py -b TRY -a 225 -c TRY -a 25223 -c TRY -a 500 -c TRY -a 4.37 -c XAU -a 80.21 -c USD
 ```
--- calc.py
+++ calc.py
@@ -11,25 +11,26 @@ BASE_URL_GOLD = f"https://api.collectapi.com/economy/goldPrice"
 
 
 def main(
-    amounts: t.Annotated[t.Optional[t.List[str]], typer.Option("-a")]=None,
+    base_currency: t.Annotated[str, typer.Option("-b")],
+    amounts: t.Annotated[t.Optional[t.List[float]], typer.Option("-a")]=None,
     currencies: t.Annotated[t.Optional[t.List[str]], typer.Option("-c")]=None,
 ):
     if amounts and currencies:
         total = 0
         for amount, currency in zip(amounts, currencies):
-            if currency == "TRY":
-                total += float(amount)
+            if currency == base_currency:
+                total += amount
                 continue
-            res = requests.get((BASE_URL_GOLD if currency == "XAU" else BASE_URL_EXCHANGE) + f"?int={amount}&base={currency}&to=TRY", headers={
+            res = requests.get((BASE_URL_GOLD if currency == "XAU" else BASE_URL_EXCHANGE) + f"?int={amount}&base={currency}&to={base_currency}", headers={
                 "Authorization": API_KEY,
                 "Content-Type": "application/json"
             })
             if currency == "XAU":
-                calcd = float(amount) * float(res.json()["result"][0]["selling"])
+                calcd = amount * float(res.json()["result"][0]["selling"])
             else:
                 calcd = float(res.json()["result"]["data"][0]["calculated"])
             total += calcd
-        print(round(total, 2))
+        print(round(total, 2), base_currency)
 
 if __name__ == "__main__":
     typer.run(main)
